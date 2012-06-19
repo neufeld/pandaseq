b77c1bd322b205af8558482442c29cc34be81b71
Andre Masella <andre@masella.name>
2012-06-18 17:58:39 -0700

    Add a plugin to allow kicking out sequences with overlap errors
    By popular demand, something which completely and utterly misses the
    point of the whole PANDAseq assembly process.

---
+++ plugins/completely_miss_the_point.c
@@ -0,0 +1,32 @@
+#define _POSIX_C_SOURCE 2
+#include<errno.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pandaseq.h>
+
+HELP("Filter out sequences that have mismatches in the overlap region.",
+     "completely_miss_the_point:mismatches");
+
+VER_INFO("1.0");
+
+int mismatches;
+
+CHECK {
+	return sequence->overlap_mismatches <= mismatches;
+}
+
+INIT {
+	if (args == NULL || *args == '\0') {
+		fprintf(stderr,
+			"Please supply the maximum allowed mismatches.\n");
+		return false;
+	}
+	errno = 0;
+	mismatches = (size_t)strtol(args, NULL, 10);
+	if (errno != 0 || mismatches < 0 || mismatches > PANDA_MAX_LEN) {
+		fprintf(stderr, "Bad maximum allowed mismatches.\n");
+		return false;
+	}
+	fprintf(stderr, "INFO\tSAMPLE\tINIT\t%s\n", args);
+	return true;
+}
