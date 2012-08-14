0e4d930963ac493637f83d50ed818cd820b6b54a
Andre Masella <andre@masella.name>
2012-08-14 18:57:09 -0400

    Added a sanity check to throw out unusably short reads

--- assembler.c
+++ assembler.c
@@ -371,6 +371,9 @@ align(PandaAssembler assembler, panda_result_seq *result)
 bool assemble_seq(PandaAssembler assembler)
 {
 	assembler->count++;
+	if (assembler->result.forward_length < 2 ||  assembler->result.reverse_length < 2) {
+		return false;
+	}
 	if (!module_precheckseq
 	    (assembler, &assembler->result.name, assembler->result.forward,
 	     assembler->result.forward_length, assembler->result.reverse,
