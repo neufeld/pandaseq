2d952c7b6eda9553e97fdfc0b86df57d52c2dbef
Andre Masella <andre@masella.name>
2013-03-04 18:38:24 -0800

    Added before and after filtering plugins

---
+++ plugins/before.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pandaseq.h>
+
+HELP("Include only sequences in the one before the provided sequence", "The sequence ID.");
+
+VER_INFO("1.0");
+
+panda_seq_identifier marker_id;
+bool state = false;
+
+PRECHECK
+{
+	if (panda_seqid_equal(&marker_id, id)) {
+		state = true;
+	}
+	return !state;
+}
+
+INIT
+{
+	if (panda_seqid_parse(&marker_id, args[0] == '@' ? (args + 1) : args, PANDA_TAG_OPTIONAL) == 0) {
+		fprintf(stderr, "ERR\tBEFORE\tBAD\t%s\n", args);
+		return false;
+	} else {
+		return true;
+	}
+}
+
