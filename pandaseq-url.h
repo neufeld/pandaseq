460c4a4ffce28397a62a7ace8bf41ffe6bf1c247
Andre Masella <andre@masella.name>
2013-11-22 19:57:10 -0800

    Make pandaseq-url.h robust
    That is, add the usual include checks and C++ externs.

--- pandaseq-url.h
+++ pandaseq-url.h
@@ -1,5 +1,3 @@
-#include <pandaseq.h>
-
 /* PANDAseq -- Assemble paired FASTQ Illumina reads and strip the region between amplification primers.
      Copyright (C) 2013  Andre Masella
 
@@ -17,7 +15,17 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
  */
-
+#ifndef _PANDASEQ_URL_H
+#        define _PANDASEQ_URL_H
+#        include <pandaseq.h>
+#        ifdef __cplusplus
+#                define EXTERN_C_BEGIN  extern "C" {
+#                define EXTERN_C_END    }
+#        else
+#                define EXTERN_C_BEGIN
+#                define EXTERN_C_END
+#        endif
+EXTERN_C_BEGIN
 /**
  * Open a URL and read the sequence.
  * @param url the URL, as understood by cURL.
@@ -45,3 +53,6 @@ bool panda_curl_ref(
  */
 void panda_curl_unref(
 	void);
+
+EXTERN_C_END
+#endif
