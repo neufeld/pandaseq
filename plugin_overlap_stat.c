b65c8c4765ee57a4bdefc54895ede71dcbb9525b
Andre Masella <andre@masella.name>
2014-04-07 19:34:28 -0400

    Add a plugin to display overlaps examined

---
+++ plugin_overlap_stat.c
@@ -0,0 +1,58 @@
+#include<pandaseq-plugin.h>
+#include<stdlib.h>
+
+HELP("Produce statistics on the overlaps examined. Somewhat interesting to see the efficiency of the k-mer table.", "overlap_stat");
+
+VER_INFO("1.0");
+
+struct data {
+	PandaWriter writer;
+	size_t counts[];
+};
+
+static bool check_func(
+	PandaLogProxy logger,
+	const panda_result_seq *sequence,
+	void *user_data) {
+
+	struct data *data = (struct data *) user_data;
+
+	data->counts[sequence->overlaps_examined]++;
+	return true;
+}
+
+static void cleanup(
+	struct data *data) {
+	size_t it;
+	size_t max;
+
+	for (max = PANDA_MAX_LEN; data->counts[max - 1] == 0; max--) ;
+
+	panda_writer_append(data->writer, "STAT\tEXAMINED");
+	for (it = 0; it < max; it++) {
+		panda_writer_append(data->writer, " %d", data->counts[it]);
+	}
+	panda_writer_append_c(data->writer, '\n');
+	panda_writer_commit(data->writer);
+	panda_writer_unref(data->writer);
+	free(data);
+}
+
+OPEN {
+	struct data *data;
+	size_t it;
+
+	if (args != NULL && args[0] != '\0') {
+		panda_log_proxy_write_str(logger, "ERR\tOVERLAPSTAT\n");
+		return false;
+	}
+	data = malloc(sizeof(struct data) + sizeof(size_t) * PANDA_MAX_LEN);
+	for (it = 1; it < PANDA_MAX_LEN; it++)
+		data->counts[it] = 0;
+	data->writer = panda_writer_ref(panda_log_proxy_get_writer(logger));
+	*precheck = NULL;
+	*check = check_func;
+	*destroy = (PandaDestroy) cleanup;
+	*user_data = data;
+	return true;
+}
